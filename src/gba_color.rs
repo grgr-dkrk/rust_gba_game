b31235a23dfadaa71fc3c92a898d6db96df958fb
grgr-dkrk <40130327+grgr-dkrk@users.noreply.github.com>
2020-04-21 22:55:01 +0900

    add gba_color

---
+++ src/gba_color.rs
@@ -0,0 +1,12 @@
+// original: https://booth.pm/ja/items/492956
+use crate::rgb::RGB;
+pub trait GBAColor {
+  fn convert_u16_color(&self) -> u16;
+}
+impl GBAColor for RGB {
+  fn convert_u16_color(&self) -> u16 {
+    return (((self.b & 0x1F) as u16) << 10)
+      + (((self.g & 0x1F) as u16) << 5)
+      + (self.r & 0x1F) as u16;
+  }
+}
