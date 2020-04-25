86b967a9fc19dccb84c55125d97f8c7d99d88380
grgr-dkrk <40130327+grgr-dkrk@users.noreply.github.com>
2020-04-25 20:39:15 +0900

    add v4

--- src/io.rs
+++ src/io.rs
@@ -1,19 +1,25 @@
 pub struct Io {
-  io_address: u32,
+  video_mode: *mut u8,
+  bg_mode: *mut u8,
 }
 
 impl Io {
   pub fn new() -> Self {
     Io {
-      io_address: 0x04000000,
+      video_mode: 0x04000000 as *mut u8,
+      bg_mode: (0x04000000 + 1) as *mut u8,
     }
   }
   pub fn screen_mode3_init(&self) {
-    let video_mode: *mut u8 = self.io_address as *mut u8;
-    let bg: *mut u8 = (self.io_address + 1) as *mut u8;
     unsafe {
-      *video_mode = 0x03;
-      *bg = 0x04;
+      *self.video_mode = 0x03;
+      *self.bg_mode = 0x04;
+    }
+  }
+  pub fn screen_mode4_init(&self) {
+    unsafe {
+      *self.video_mode = 0x04;
+      *self.bg_mode = 0x04;
     }
   }
 }
